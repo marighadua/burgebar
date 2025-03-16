b088a0337c383ac61b9d6c24f217bd6ef9c88495
marighadua <ghadua96@gmail.com>
2025-03-16 19:47:32 +0100

    burgerbar

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let navBar = document.getElementById("navBar");
+let burgerBar = document.getElementById("burgerBar");
+
+burgerBar.addEventListener("click", function () {
+    navBar.classList.toggle("newNav");
+ })
